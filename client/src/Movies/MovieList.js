eeecb895bcdf9d7bff90cc45aec6387bad90a5b4
zachary-peasley <zpman95@hotmail.com>
2019-08-20 15:40:56 -0500

    fixed error on MovieList, MVP

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -23,9 +23,9 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <Link to ={`/movies/${movie.id}`}>
+  
         <MovieDetails key={movie.id} movie={movie} />
-        </Link>
+        
       ))}
     </div>
   );
